d4c8fa02b85c3a34726ee4f46063339c30e6c2f5
lviken <larserikviken@gmail.com>
2021-08-13 12:03:00 +0200

    Virker, fortsatt et par småting skal endres

--- src/App.js
+++ src/App.js
@@ -5,28 +5,83 @@ import CurrencyRow from './CurrencyRow';
 const BASE_URL = 'https://v6.exchangerate-api.com/v6/e3d0522704ab828b2ba21ea5/latest/USD'
 
 function App() {
-
  
   const [currencyOptions, setCurrencyOptions] = useState([])
-  console.log(currencyOptions)
+  const [fromCurrency, setFromCurrency] = useState()
+  const [toCurrency, setToCurrency] = useState()
+  const [exchangeRate, setExchangeRate] = useState()
+  const [amount, setAmount] = useState(1)
+  const [amountInFromCurrency, setAmountInFromCurrency] = useState(true)
+
+  let toAmount, fromAmount
+  if(amountInFromCurrency) {
+    fromAmount = amount
+    toAmount = amount * exchangeRate
+  } else {
+    toAmount = amount
+    fromAmount = amount / exchangeRate
+  }
+
+ 
+
   useEffect(() => {
     fetch(BASE_URL)
     .then(res => res.json())
     .then(data => {
-      setCurrencyOptions([data.base_code, ...Object.keys(data.conversion_rates)]) 
+      const firstCurrency = Object.keys(data.conversion_rates)[1]
+      setCurrencyOptions([(data.base_code), ...Object.keys(data.conversion_rates)])
+      setFromCurrency(data.base_code)
+      setToCurrency (firstCurrency)
+      setExchangeRate(data.conversion_rates[firstCurrency])
+
 
     })
 
 
   }, [] ) // tomt array sørger for at useEffect bare kalles den første gangen vi laster api
 
+  function handleFromAmountChange(e) { 
+    setAmount(e.target.value)
+    setAmountInFromCurrency(true)
+
+  }
+
+  useEffect(() => {
+    if(fromCurrency != null && toCurrency != null){
+      fetch (`${BASE_URL}?base=${fromCurrency}&symbols=${toCurrency}`)
+      .then(res => res.json())
+      .then(data => setExchangeRate(data.conversion_rates[toCurrency]))
+    }
+    
+
+  }, [fromCurrency, toCurrency])
+
+  function handleToAmountChange(e) { 
+    setAmount(e.target.value)
+    setAmountInFromCurrency(false)
+
+  }
+
   return (
 
     <>
    <h1>Convert</h1>
-   <CurrencyRow />
+   <CurrencyRow
+   currencyOptions = {currencyOptions} 
+    selectedCurrency ={fromCurrency}
+    onChangeCurrency = {e => setFromCurrency(e.target.value)}
+    onChangeAmount = {handleFromAmountChange}
+    amount = {fromAmount}
+    />
+
    <div className="equals">=</div>
-   <CurrencyRow />
+   <CurrencyRow
+   currencyOptions = {currencyOptions}
+   selectedCurrency = {toCurrency}
+   onChangeCurrency = {e => setToCurrency (e.target.value)}
+   onChangeAmount = {handleToAmountChange}
+   amount = {toAmount}
+    />
    </>
   );
 }
